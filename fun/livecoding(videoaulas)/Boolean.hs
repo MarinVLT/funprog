3b31fbf367ae0f2b87d4eafbae6b0e9fd62a4dc5
Marina Torres <64992110+MarinVLT@users.noreply.github.com>
2022-09-07 16:01:16 -0500

    mudando arq Boolean.hs

---
+++ fun/livecoding(videoaulas)/Boolean.hs
@@ -0,0 +1,19 @@
+module Boolean where
+
+data Boolean = F | T deriving (Show)
+
+lor :: Boolean -> Boolean -> Boolean 
+lor F F = F
+lor _ _ = T
+
+land :: Boolean -> Boolean -> Boolean
+land T T = T
+land _ _ = F
+
+--lor :: (Boolean,Boolean) -> Boolean
+--lor (F,F) = F
+--lor _ = T
+
+--land :: (Boolean,Boolean) -> Boolean
+--land (T,T) = T
+--land _ = F
