c0840bc97929245e2ff725840fbdd60817624251
Marina Torres <64992110+MarinVLT@users.noreply.github.com>
2022-09-07 16:47:50 -0500

    att Tipos.hs

--- fun/video_aulas/Tipos.hs
+++ fun/video_aulas/Tipos.hs
@@ -9,9 +9,29 @@ module Tipos where
 --(->) (pega dois tipos e vira um novo tipo - funções)'
 
 --criando um type
-data Weekday = Mon | Tue | Wed | Thu | Fri | Sat | Sun deriving (Show)
--- deriving (Show) deixa para o haskell implementar o show (print)
+data Weekday = Mon | Tue | Wed | Thu | Fri | Sat | Sun  deriving (Show, Eq)
+-- deriving (Show) deixa para o haskell implementar o show (print); deriving (Eq) deixa para o haskell implementar a Eq (equidade)
 
+-- definido Eq na mão
+-- instance (Eq Weekday) where
+-- Mon == Mon = True
+-- Tue == Tue = True
+-- Wed == Wed = True
+-- Thu == Thu = True
+-- Fri == Fri = True
+-- Sat == Sat = True
+-- Sun == Sun = True
+-- _ == _ = False
+
+-- definindo Show na mão
+-- instance (Show Weekday) where
+-- show Mon = "Monday"
+-- show Tue = "Tuesday"
+-- show Wed = "Wednesday"
+-- show Thu = "Thursday"
+-- show Fri = "Friday"
+-- show Sat = "Saturday"
+-- show Sun = "Sunday"
 
 nextday :: Weekday -> Weekday
 nextday Mon = Tue
@@ -35,4 +55,5 @@ nextWorkingDay _ = Mon
 --nextWorkingDay Sun = Mon
 --nextWorkingDay x = nextday x
 
-
+isHoliday :: Weekday -> Weekday
+isHoliday = undefined 
